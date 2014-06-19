a0da9dd7d77d6471a84bb3a22eb8a4b5a4d20ed3
Stuart Nelson <stuartnelson3@gmail.com>
2014-06-19 10:57:56 -0400

    gofmt and update to new eventsource package

--- server.go
+++ server.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-    eventsource "github.com/antage/eventsource/http"
-    "github.com/martini-contrib/cors"
-    "github.com/codegangsta/martini"
-    "net/http"
-    "strconv"
-    "os"
+	eventsource "github.com/antage/eventsource"
+	"github.com/codegangsta/martini"
+	"github.com/martini-contrib/cors"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
-    id := 1
-    es := eventsource.New(
-        eventsource.DefaultSettings(),
-        func(req *http.Request) [][]byte {
-            return [][]byte{
-                []byte("X-Accel-Buffering: no"),
-                []byte("Access-Control-Allow-Origin: *"),
-            }
-        },
-    )
-    defer es.Close()
+	id := 1
+	es := eventsource.New(
+		eventsource.DefaultSettings(),
+		func(req *http.Request) [][]byte {
+			return [][]byte{
+				[]byte("X-Accel-Buffering: no"),
+				[]byte("Access-Control-Allow-Origin: *"),
+			}
+		},
+	)
+	defer es.Close()
 
-    m := martini.Classic()
-    m.Use(cors.Allow(&cors.Options{
-        AllowOrigins:     []string{"http://*", "https://*"},
-        AllowMethods:     []string{"GET"},
-        AllowHeaders:     []string{"Origin"},
-    }))
-    m.Get("/stream", func(w http.ResponseWriter, r *http.Request) {
-        token := r.FormValue("token")
-        if token != os.Getenv("TOKEN") {
-            return
-        }
-        es.ServeHTTP(w, r)
-    })
-    m.Post("/update_stream", func(w http.ResponseWriter, r *http.Request) {
-        token := r.FormValue("token")
-        if token != os.Getenv("TOKEN") {
-            return
-        }
-        card := r.FormValue("card")
-        stream := r.FormValue("stream")
-        es.SendMessage(card, stream, strconv.Itoa(id))
-        id++
-    })
-    m.Run()
+	m := martini.Classic()
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins: []string{"http://*", "https://*"},
+		AllowMethods: []string{"GET"},
+		AllowHeaders: []string{"Origin"},
+	}))
+	m.Get("/stream", func(w http.ResponseWriter, r *http.Request) {
+		token := r.FormValue("token")
+		if token != os.Getenv("TOKEN") {
+			return
+		}
+		es.ServeHTTP(w, r)
+	})
+	m.Post("/update_stream", func(w http.ResponseWriter, r *http.Request) {
+		token := r.FormValue("token")
+		if token != os.Getenv("TOKEN") {
+			return
+		}
+		card := r.FormValue("card")
+		stream := r.FormValue("stream")
+		es.SendEventMessage(card, stream, strconv.Itoa(id))
+		id++
+	})
+	m.Run()
 }
